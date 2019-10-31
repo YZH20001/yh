0733da4edab5ef28e05a68228a78ee7e136f0cb7
YH <3274501813@qq.com>
2019-10-31 22:17:43 +0800

    修改样式

---
+++ BolgSystemMVCSite/Scripts/index.js
@@ -0,0 +1,47 @@
+﻿var createM = document.querySelector(".create"),
+    showOrHidden = document.querySelector(".showorhidden"),
+    sanJiao = document.querySelector("#sanjiao"),
+    articleA = document.querySelector(".article"),
+    sanjiaoArticle = document.querySelector("#sanjiaoarticle"),
+    showorHiddenArticle = document.querySelector("#showorhiddenarticle");
+
+console.log(sanjiaoArticle);
+init();
+function init() {
+    createM.addEventListener("mouseover", mouseroverHandler);
+    createM.addEventListener("mouseout", mouseroutHandler);
+    articleA.addEventListener("mouseover", mouseroverArticleHandler);
+    articleA.addEventListener("mouseout", mouseroutArticleHandler);
+    createM.addEventListener("click", clickHandler);
+    articleA.addEventListener("click", clickArticleHandler);
+}
+function mouseroverHandler() {
+    createM.style.background = "aqua";
+}
+function mouseroutHandler() {
+    createM.style.background = "none";
+}
+function mouseroverArticleHandler() {
+    articleA.style.background = "aqua";
+}
+function mouseroutArticleHandler() {
+    articleA.style.background = "none";
+}
+function clickHandler() {
+    if (showOrHidden.style.display == "block") {
+        showOrHidden.style.display = "none";
+        sanJiao.style.transform = "rotate(0)";
+    } else {
+        showOrHidden.style.display = "block";
+        sanJiao.style.transform = "rotate(180deg)";
+    }
+}
+function clickArticleHandler() {
+    if (showorHiddenArticle.style.display == "block") {
+        showorHiddenArticle.style.display = "none";
+        sanjiaoArticle.style.transform = "rotate(0)";
+    } else {
+        showorHiddenArticle.style.display = "block";
+        sanjiaoArticle.style.transform = "rotate(180deg)";
+    }
+}
