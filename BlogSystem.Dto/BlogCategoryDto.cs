379d87135f9f411d26badd74d81f5225313f2d1b
YH <3274501813@qq.com>
2019-11-02 13:37:10 +0800

    修改样式

--- BlogSystem.Dto/BlogCategoryDto.cs
+++ BlogSystem.Dto/BlogCategoryDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace BlogSystem.Dto
     public class BlogCategoryDto
     {
         public Guid Id { get; set; }
+        [Display(Name = "文章分类名")]
         public  string CategoryName { get; set; }
     }
 }
